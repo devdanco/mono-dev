dfcb5c6bd84f0ac4d587e57e616e7ccac6db05f5
devdanco <devdanco@gmail.com>
2023-09-14 10:00:56 +0200

    fix: dadads

--- release.config.js
+++ release.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   branches: ['main', { 'name': 'rc/*', 'channel': 'rc', 'prerelease': 'rc' }],
   preset: 'conventionalcommits',
+  presetConfig: {
+    types: [
+      { type: 'feat', section: 'Features' },
+      { type: 'fix', section: 'Bug Fixes' },
+      { type: 'chore', section: 'Chores' },
+      { type: 'docs', hidden: true },
+      { type: 'style', hidden: true },
+      { type: 'refactor', section: 'Refactoring' },
+      { type: 'perf', hidden: true },
+      { type: 'test', hidden: true },
+    ],
+  },
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
