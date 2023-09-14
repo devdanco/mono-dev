64cc97419eb7a34ace53c7cbdd1a92e4f099b788
devdanco <devdanco@gmail.com>
2023-09-14 09:54:39 +0200

    fix: dsadsadsada

--- release.config.js
+++ release.config.js
@@ -1,19 +1,6 @@
 module.exports = {
-  branches: ['main', { "name": "rc/*", "prerelease": true }],
+  branches: ['main', { 'name': 'rc/*', 'channel': 'rc', 'prerelease': 'rc' }],
   preset: 'conventionalcommits',
-  presetConfig: {
-    types: [
-      { type: 'feat', section: 'Features' },
-      { type: 'fix', section: 'Bug Fixes' },
-      { type: 'chore', section: 'Chores' },
-      { type: 'docs', hidden: true },
-      { type: 'style', hidden: true },
-      { type: 'refactor', section: 'Refactoring' },
-      { type: 'perf', hidden: true },
-      { type: 'test', hidden: true },
-    ],
-  },
-  releaseRules: [{ type: 'refactor', release: 'patch' }],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
